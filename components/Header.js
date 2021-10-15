317c177aa9fab849dba001a68082cfc7bd488b49
Shakthi08 <75329522+Shakthi08@users.noreply.github.com>
2021-10-15 19:12:52 +0100

    donedone

--- components/Header.js
+++ components/Header.js
@@ -27,7 +27,7 @@ function Header() {
 				<Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
 				<div className="flex ml-2 items-center rounded-full bg-gray-100 p-2 ">
 					<SearchIcon className='h-6 text-gray-600' />
-					<input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook"/>
+					<input className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook"/>
 				</div>
 			</div>
 
@@ -50,10 +50,15 @@ function Header() {
 
 			{/* Right */}
 
-			<div>
+			<div className="flex items-center sm:space-x-2 justify-end">
 				{/*Profile pic*/}
 
 				<p className="whitespace-nowrap font-semibold pr-3">Dev Shakthi. B</p>
+				<ViewGridIcon className="icon"/>
+				<ChatIcon className="icon"/>
+				<BellIcon className="icon"/>
+				<ChevronDownIcon className="icon"/>
+				
 
 
 			</div>

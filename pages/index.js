92e3e392295d1021456c9a1a844d98f176c101ca
Shakthi08 <75329522+Shakthi08@users.noreply.github.com>
2021-10-19 09:20:42 +0100

    login

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
 import Header from "../components/Header";
+import { getSession } from "next-auth/client";
+import Login from "../components/Login";
+
+export default function Home({ session }) {
+
+  if(!session) return <Login  />;
 
 
-
-export default function Home() {
   return (
     <div>
       <Head>
@@ -25,4 +29,16 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  //Get the user
+  const session = await getSession(context);
+
+  return{
+    props:{
+      session
+    }
+  }
+
 }

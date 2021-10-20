7611bbfb71c92d73e3335d05bc0468151fb7097f
Shakthi08 <75329522+Shakthi08@users.noreply.github.com>
2021-10-20 17:17:42 +0100

    d

--- components/Feed.js
+++ components/Feed.js
@@ -4,8 +4,8 @@ function Feed(){
     return(
 
 
-        <div className="flex-grow h-screen">
-            <div>
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+            <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
                 {/* Stories */}
 
                 

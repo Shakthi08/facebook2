04e7de3def53ed26a2280ff1f32dbe6d9b550190
Shakthi08 <75329522+Shakthi08@users.noreply.github.com>
2021-10-20 17:08:51 +0100

    newww

---
+++ components/Feed.js
@@ -0,0 +1,24 @@
+import Stories from "./Stories"
+
+function Feed(){
+    return(
+
+
+        <div className="flex-grow h-screen">
+            <div>
+                {/* Stories */}
+
+                
+                <Stories />
+
+
+                {/* Input box */}
+                {/* Posts */}
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Feed
---
+++ components/StoryCard.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+
+function StoryCard({ src, name, profile }) {
+    return(
+
+        <div className="relative h-14 w=14">
+            <Image
+                className="absolute opacity-0 lg-opacity-100 rounded-full z-50 top-10"
+                src={profile}
+                width={50}
+                height={40}
+                layout="fixed"
+                objectFit="cover"
+            />
+            <Image
+                className="object-cover fliter brightness-75 rounded-full lg:rounded-3xl"
+                src={src}
+                layout="fill"
+            />
+        </div>
+
+    )
+}
+
+
+export default StoryCard

12e5d8306ba9f72e68c9bacaaf1bb15413363e32
Shakthi08 <75329522+Shakthi08@users.noreply.github.com>
2021-10-21 20:09:02 +0100

    ib

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,22 +1,61 @@
-import Image from "next/Image";
+import Image from "next/image";
 import { useSession } from "next-auth/client";
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+
+
 
 function InputBox(){
 
-    const [session] = useSession{};
+    const [session] = useSession();
+    const sendPost = (e) => {
+        e.preventDefault();
+    };
 
-    return <div>
-        <div>
-            <Image 
-                className="rounded-full"
-                src={session.user.image}
-                width={40}
-                height={40}
-                layout="fixed"
-            />
+    return (
+        <div className="bg-white rounded-2xl p-2 shadow-md text-gray-500 font-medium mt-6">
+            <div className="flex space-x-4 p-4 items-center">
+                <Image 
+                    className="rounded-full"
+                    src={session.user.image}
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                />
+
+
+                <form className="flex flex-1">
+                    <input className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"  type="text" placeholder={`What's on your mind, ${session.user.name}`}></input>
+                    <button hidden onClick={sendPost} type="submit" >Post</button>
+                </form>
+
+
+            </div>
+
+
+            <div className="flex justify-evenly p-3 border-t">
+
+                <div className="inputIcon">
+                    <VideoCameraIcon className="h-7 text-red-500"/>
+                    <p className="text-xs sm:text-sm xl: text-base">Live video</p>
+                </div>
+
+                <div>
+                    <CameraIcon className="h-7 text-green-400"/>
+                    <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+                </div>
+
+                <div>
+                    <EmojiHappyIcon className="h-7 text-yellow-300"/>
+                    <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
+                </div>
+
+
+            </div>
+
+
         </div>
-    </div>;
-
+    );
 }
 
 export default InputBox;
